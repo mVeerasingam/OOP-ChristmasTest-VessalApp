13812142d17b2121307a6c1c8c10e22f44a8dc30
G00403931 <G00403931@atu.ie>
2022-12-12 09:38:08 +0000

    added feature_ferry, ferry class and completed part (b)

---
+++ src/main/java/ie/atu/week12/christmasTest/Ferry.java
@@ -0,0 +1,19 @@
+package ie.atu.week12.christmasTest;
+
+public class Ferry extends Ship{
+    private int maxPassengers;
+
+    public Ferry(int maxPassengers) {
+        this.maxPassengers=maxPassengers;
+    }
+
+    public Ferry(String name, String yearBuilt, int numOfCrew, int maxPassengers) {
+        super(name, yearBuilt, numOfCrew);
+        this.maxPassengers = maxPassengers;
+    }
+
+    @Override
+    public String vesselDetails() {
+        return String.format("MaximumPassengers: [%d]",maxPassengers);
+    }
+}
--- src/main/java/ie/atu/week12/christmasTest/VessalApp.java
+++ src/main/java/ie/atu/week12/christmasTest/VessalApp.java
@@ -3,7 +3,9 @@ package ie.atu.week12.christmasTest;
 public class VessalApp {
     public static void main(String[] args) {
         Ship ship = new Ship("SS. Plennon", "1912", 111);
-
         System.out.println(ship.vesselDetails());
+
+        Ferry ferry = new Ferry(87);
+        System.out.println(ferry.vesselDetails());
     }
 }
